bcfebe328ad81984d131d276a445f9a3469383e7
Michelle Morin <morin2m@gmail.com>
2020-03-24 09:46:42 -0700

    add library context  model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

---
+++ Library/Models/LibraryContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models
+{
+  public class LibraryContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Author> Authors {get;set;}
+    public DbSet<Book> Books {get;set;}
+    public DbSet<BookAuthor> BookAuthor {get;set;}
+    public DbSet<Copy> Copies {get;set;}
+    public DbSet<Checkout> Checkouts {get;set;}
+    public DbSet<Patron> Patrons {get;set;}
+    public LibraryContext(DbContextOptions options) : base(options) { }
+  }
+}
