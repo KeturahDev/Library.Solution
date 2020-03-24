57850892bccf239912366bdf834150dbed94e4b8
Michelle Morin <morin2m@gmail.com>
2020-03-24 09:30:33 -0700

    add patron model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

---
+++ Library/Models/Patron.cs
@@ -0,0 +1,13 @@
+namespace Library.Models
+{
+  public class Patron
+  {
+    public Patron()
+    {
+      this.Checkouts = new Hashset<Checkout>();
+    }
+    public int PatronId {get;set;}
+    public string Name {get;set;}
+    public ICollection<Checkout> Checkouts {get;set;}
+  }
+}
