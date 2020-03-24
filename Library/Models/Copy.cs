0d9636cd4ede7b4b4e6b226d280f43e44cb0e957
Michelle Morin <morin2m@gmail.com>
2020-03-24 13:17:10 -0700

    update copy model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- Library/Models/Copy.cs
+++ Library/Models/Copy.cs
@@ -5,6 +5,7 @@ namespace Library.Models
   public class Copy
   {
     public int CopyId {get;set;}
+    public bool Available {get; set;}
     public int BookId {get;set;}
     public virtual Book Book { get; }
   }
