344f9dad0c66e0b7c7de1b2e8e991eef2d5c090d
Michelle Morin <morin2m@gmail.com>
2020-03-24 09:24:53 -0700

    add book model
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>
    Co-authored by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

---
+++ Library/Models/Book.cs
@@ -0,0 +1,13 @@
+namespace Library.Models
+{
+  public class Book
+  {
+    public Book()
+    {
+      this.Authors = new Hashset<BookAuthor>();
+    }
+    public int BookId {get;set;}
+    public string Title {get;set;}
+    public ICollection <BookAuthor> Authors { get; }
+  }
+}
